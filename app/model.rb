a35b222162c0765e6568d23d7b5783577c2f028f
moonpxi <paulo.schneider@gmail.com>
2009-12-20 20:38:01 -0200

    Generates the link url when creating the post

--- app/model.rb
+++ app/model.rb
@@ -12,16 +12,28 @@ class Post
                               :presence  => "Title is required",
                               :is_unique => "Title must be unique"
                            }
+  property :link, String
   property :content, Text, :nullable => false, :message => "Content is required"
   property :created_at, DateTime
+  
+  def initialize(hash = {})
+    super(hash)
+    self.link = title ? title.downcase.split.join("_") : "invalid_post"
+  end
   
   def formatted_content
     RDiscount.new(content).to_html
   end
 
   def summary
-    content = formatted_content
-    heading_index = content.index("<h2>")
-    heading_index ? content[0..heading_index - 1] : content
+    summary_content = formatted_content
+    heading_index = summary_content.index("<h2>") 
+    heading_index ? summary_content[0..heading_index - 1] + read_more_link : summary_content
+  end
+  
+  private
+  
+  def read_more_link
+    "<a href=\"post/#{link}\">Read more...</a>" # TODO: move this to the view
   end
 end
--- spec/model_spec.rb
+++ spec/model_spec.rb
@@ -2,46 +2,61 @@ require "spec_helper"
  
 describe "Model" do
   
+  SUMMARY_CONTENT_EXAMPLE = <<eos
+The summary goes from __here__ until...
+
+...the start of a heading.
+
+This is the end of the summary
+-----------------
+
+This should not appear
+eos
+  
   before(:each) do
     Post.all.destroy!
   end
   
-  it "should fail validation if Post have no title" do
-    post = Post.new(:title => nil)
-    
-    post.should_not be_valid
-    post.errors.should have_key(:title)
-    post.errors[:title].should == ["Title is required"]
-  end
+  describe "(validations)" do
   
-  it "should fail validation if Post title is not unique" do
-    Post.new(:title => "some title", :content => "first post").save
-    post = Post.new(:title => "some title", :content => "second post")
+    it "should fail validation if Post have no title" do
+      post = Post.new(:title => nil)
     
-    post.should_not be_valid
-    post.errors.should have_key(:title)
-    post.errors[:title].should == ["Title must be unique"]
-  end
+      post.should_not be_valid
+      post.errors.should have_key(:title)
+      post.errors[:title].should == ["Title is required"]
+    end
   
-  it "should fail validation if Post have no content" do
-    post = Post.new(:title => "Some title")
+    it "should fail validation if Post title is not unique" do
+      Post.new(:title => "some title", :content => "first post").save
+      post = Post.new(:title => "some title", :content => "second post")
     
-    post.should_not be_valid
-    post.errors.should have_key(:content)
-    post.errors[:content].should == ["Content is required"]
-  end
+      post.should_not be_valid
+      post.errors.should have_key(:title)
+      post.errors[:title].should == ["Title must be unique"]
+    end
   
-  it "should save properly constructed Post" do
-    post = Post.new(:title => "Blah", :content => "This is the content of the post. Yay!")
+    it "should fail validation if Post have no content" do
+      post = Post.new(:title => "Some title")
     
-    post.should be_valid
-    post.save.should be_true
-    post.title.should == "Blah"
-    post.content.should == "This is the content of the post. Yay!"
-    post.created_at.should_not be_nil # FIXME: better date check
+      post.should_not be_valid
+      post.errors.should have_key(:content)
+      post.errors[:content].should == ["Content is required"]
+    end
+  
+    it "should save properly constructed Post" do
+      post = Post.new(:title => "Blah", :content => "This is the content of the post. Yay!")
+    
+      post.should be_valid
+      post.save.should be_true
+      post.title.should == "Blah"
+      post.content.should == "This is the content of the post. Yay!"
+      post.created_at.should_not be_nil # FIXME: better date check
+    end
   end
 
   describe "(formatting)" do
+    
     it "should allow markdown formatting in the content"  do
       markdown_content = <<eos
 This is a heading
@@ -57,19 +72,9 @@ eos
     end
 
     it "should display the content until the first heading as summary" do
-       markdown_content = <<eos
-The summary goes from __here__ until...
-
-...the start of a heading.
-
-This is the end of the summary
------------------
-
-This should not appear
-eos
-      post = Post.new(:content => markdown_content)
+      post = Post.new(:content => SUMMARY_CONTENT_EXAMPLE)
       
-      post.summary.should == "<p>The summary goes from <strong>here</strong> until...</p>\n\n<p>...the start of a heading.</p>\n\n"
+      post.summary.should == "<p>The summary goes from <strong>here</strong> until...</p>\n\n<p>...the start of a heading.</p>\n\n<a href=\"post/invalid_post\">Read more...</a>"
     end
 
     it "should show whole post as summary in case there is no headings" do
@@ -79,7 +84,22 @@ eos
     end
 
     it "should show a 'Read More' link when displaying a summary" do
-      pending
+      post = Post.new(:title => "Read more link here", :content => SUMMARY_CONTENT_EXAMPLE)
+      
+      post.summary.include?("<a href=\"post/read_more_link_here\">Read more...</a>").should be_true
+    end
+  end
+  
+
+  describe "(linking)" do
+    it "should have a title-based link" do
+      post = Post.new(:title => "This is a example 32 of a link")
+      
+      post.link.should == "this_is_a_example_32_of_a_link"
+    end
+    
+    it "should have a 'invalid_post' link when title is missing" do
+      Post.new.link.should == "invalid_post"
     end
   end
 
